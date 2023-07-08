a52f47692baed3ea44b88bb1f3695d0c0d86976c
Bogomil Simitchiev <simitchiev365@abv.bg>
2023-07-08 17:00:47 +0300

    Exercised 'Sequelize' logic and rewrote some functionalities

--- Understanding Sequelize/util/database.js
+++ Understanding Sequelize/util/database.js
@@ -1,6 +1,6 @@
 const Sequelize = require('sequelize');
 
-const sequelize = new Sequelize('node-complete', 'root', 'youngdeveloper', {
+const sequelize = new Sequelize('node-complete', 'root', 'Here is your password in workbench', {
   dialect: 'mysql',
   host: 'localhost'
 });
