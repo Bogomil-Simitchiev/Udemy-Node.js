7fff1efe6bd609d7ce660a61e255c77b06d2b11f
Bogomil Simitchiev <simitchiev365@abv.bg>
2023-08-04 19:34:36 +0300

    Added authentication

--- Sending Emails/routes/auth.js
+++ Sending Emails/routes/auth.js
@@ -1,17 +1,20 @@
 const express = require('express');
 
 const authController = require('../controllers/auth');
+const isGuest = require('../middleware/is-guest');
+const isAuth = require('../middleware/is-auth');
+
 
 const router = express.Router();
 
-router.get('/login', authController.getLogin);
+router.get('/login', isGuest, authController.getLogin);
 
-router.get('/signup', authController.getSignup);
+router.get('/signup', isGuest, authController.getSignup);
 
-router.post('/login', authController.postLogin);
+router.post('/login', isGuest, authController.postLogin);
 
-router.post('/signup', authController.postSignup);
+router.post('/signup', isGuest, authController.postSignup);
 
-router.post('/logout', authController.postLogout);
+router.post('/logout', isAuth, authController.postLogout);
 
 module.exports = router;
